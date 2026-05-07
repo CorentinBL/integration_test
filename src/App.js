e0ab2223bacc15e8f558586cba80464d00c67c47
LamaShin <c.blairon-lacoste@hotmail.fr>
2026-05-07 10:03:54 +0200

    feat:counter App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  let [count,setCount] = useState(0);
+  const clickOnMe = () => {
+    setCount(count + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <button onClick={clickOnMe}>Click me</button>
+        <span data-testid="count">{count}</span>
+      </div>
   );
-}
+  }
 
-export default App;
+  export default App;
