25caff56ef0b08ccd08a6c5468c8701849a7963e
LamaShin <c.blairon-lacoste@hotmail.fr>
2026-06-18 14:40:08 +0200

    chore:added Docker setup architecture

---
+++ sqlfiles/migrate-v001.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE ynov_ci;
+USE ynov_ci;
+CREATE TABLE IF NOT EXISTS utilisateurs
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(100),
+    prenom VARCHAR(100),
+    email VARCHAR(255),
+    date_naissance DATE,
+    pays VARCHAR(255),
+    ville VARCHAR(255),
+    code_postal VARCHAR(5)
+);
+DESCRIBE utilisateurs;
+
---
+++ sqlfiles/migrate-v002.sql
@@ -0,0 +1,3 @@
+USE ynov_ci;
+INSERT INTO utilisateurs (nom, prenom, email)
+VALUES ("fenoll", "loise", "loise.fenoll@ynov.com");
